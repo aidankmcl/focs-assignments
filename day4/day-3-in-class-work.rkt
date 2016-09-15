8fa03e520b6bc9e1b533d890a6670d982a70eba2
Aidan <aidankmcl@gmail.com>
2016-09-15 11:15:28 -0400

    hw3!

--- day4/day-3-in-class-work.rkt
+++ day4/day-3-in-class-work.rkt
@@ -7,6 +7,22 @@
 ;; 0.  Implement factorial both recursively and tail recursively.
 ;;     Hint:  The tail recursive version will use a helper function.
 
+(define (fact-rec num)
+  (if (zero? (- num 1))
+      1
+      (* num (fact-rec (- num 1)))))
+
+(display (fact-rec 4)) (newline) ;; --> 24
+
+(define (fact-tail num)
+  (tail-helper num 1))
+
+(define (tail-helper num total)
+  (if (zero? (- num 1))
+      total
+      (* num (tail-helper (- num 1) total))))
+
+(display (fact-tail 4)) (newline) ;; --> 24
 
 ;;;;;;;;;;;
 ;; 1.  Filter is built in to scheme.
@@ -18,9 +34,15 @@
 
 ;; Implement it anyway.  You might want to call it my-filter?  What arguments does it take?
 
+(define (filter-check proc arg-list filtered-list)
+  (cond [(null? arg-list) (reverse filtered-list)]
+        [(proc (first arg-list)) (filter-check proc (rest arg-list) (cons (first arg-list) filtered-list))]
+        [else (filter-check proc (rest arg-list) filtered-list)]))
 
+(define (my-filter proc arg-list)
+  (filter-check proc arg-list '()))
 
-
+(display (my-filter odd? `(1 2 3 4 5 6))) (newline) ;; --> (1 3)
 
 ;;;;;;;;;;;
 ;; 2.  Map is also built in to scheme.
@@ -35,10 +57,16 @@
 
 ;; Implement it as well.  You might want to call it my-map.  What arguments does it take?
 
+(define (my-map-helper proc arg-list results)
+  (if (null? arg-list)
+      (reverse results)
+      (my-map-helper proc (rest arg-list) (cons (proc (first arg-list)) results))))
 
+(define (my-map proc arg-list)
+  (my-map-helper proc arg-list '()))
 
-
-
+(define (double x) (* 2 x))
+(display (my-map double '(1 2 3))) (newline) ;; --> (2 4 6)
 
 
 ;;;;;;;;;;;
@@ -51,35 +79,76 @@
 ;; You might want to draw out the box and pointer structures for the original two lists
 ;; as well as for the new list.  Confirm with a member of the instructional staff....
 
+(define (my-append-homie content so-far)
+	(if (null? content)
+		so-far
+		(my-append-homie (rest content) (cons (first content) so-far))))
 
+(define (my-append first-lst second-lst)
+	(reverse (my-append-homie second-lst (my-append-homie first-lst '()))))
+
+(display (my-append '(1 2 3) '(4 5 6))) (newline) ;; (1 2 3 4 5 6)
 
 
 
 ;;;;;;;;;;;
 ;; 4.  zip takes two lists, and returns a list of elements of size two, until one of the lists runs out.
-
-;; (zip '(1 2 3) '(4 5 6)) ;; --> '((1 4) (2 5) (3 6))
-;; (zip '(1 2 3) '(a b c d e f g)) ;; --> '((1 a) (2 b) (3 c))
-
 ;; Implement `zip`.
 
+(define (my-zip-homie first-lst second-lst so-far)
+	(if (or (null? first-lst) (null? second-lst))
+		so-far
+		(my-zip-homie (rest first-lst) (rest second-lst) (cons (list (first first-lst) (first second-lst)) so-far))))
 
+(define (my-zip first-lst second-lst)
+	(reverse (my-zip-homie first-lst second-lst '())))
+
+(display (my-zip '(1 2 3) '(4 5 6))) (newline) ;; --> '((1 4) (2 5) (3 6))
+(display (my-zip '(1 2 3) '(a b c d e f g))) (newline) ;; --> '((1 a) (2 b) (3 c))
 
 
 ;;;;;;;;;;;;
 ;; 5.  Last built-in (for now):  (my-)reverse.
 ;;     Takes a list, returns a new list with the elements reversed.
 
-;; (reverse '(1 2 3)) --> '(3 2 1)
+(define (my-reverse-homie lst so-far)
+	(if (null? lst)
+		so-far
+		(my-reverse-homie (rest lst) (cons (first lst) so-far))))
 
+(define (my-reverse lst)
+	(my-reverse-homie lst '()))
 
+(display (my-reverse '(1 2 3))) (newline) ;; --> '(3 2 1)
 
 ;;;;;;;;;;;;
 ;; More puzzles:
-;;
 ;;  - (count elt lst) returns the number of times that elt appears in lst
+(define (count-helper elt lst num)
+	(cond [(null? lst) num]
+		  [(eq? elt (first lst)) (count-helper elt (rest lst) (+ num 1))]
+		  [else (count-helper elt (rest lst) num)]))
+
+(define (count elt lst)
+	(count-helper elt lst 0))
+
+(display (count 'MEOW (list 'MEOW 'CAT 'MEOW) )) (newline) ;; --> 2
+
 ;;  - (remove-dups lst) returns a new list that contains the elements of lst but without repeats
-;;       (remove-dups '(1 2 3 1 4 5 2 4 6)) --> '(1 2 3 4 5 6)
+
+(define (remove-dups-helper lst total)
+	(cond [(null? lst) (reverse total)]
+		  [(> (count (first lst) total) 0) (remove-dups-helper (rest lst) total)]
+		  [else (remove-dups-helper (rest lst) (cons (first lst) total))]))
+
+(define (remove-dups lst)
+	(remove-dups-helper lst '()))
+
+(display (remove-dups (list 1 2 3 1 4 5 2 4 6))) (newline) ;; --> '(1 2 3 4 5 6)
+
 ;;  - reverse reverses a list, but doesn't reverse sublists in a tree.  (Try it and see.)
+(display (my-reverse '(1 2 (2 1 4) 3))) (newline) ;; --> (3 (2 1 4) 2 1)
+
 ;;    Write deep-reverse, which reverses all sublists as well.
+; display (my-deep-reverse '(1 2 (2 1 4) 3))) (newline)
 ;;  - Which of these can you implement using tail recursion?
--- day4/hw3.rkt
+++ day4/hw3.rkt
@@ -1,9 +1,9 @@
 #lang racket
 
-;;; Student Name: Frankly Olin [change to your name]
+;;; Student Name: Aidan McLaughlin
 ;;;
 ;;; Check one:
-;;; [ ] I completed this assignment without assistance or external resources.
+;;; [X] I completed this assignment without assistance or external resources.
 ;;; [ ] I completed this assignment with assistance from ___
 ;;;     and/or using these external resources: ___
 
@@ -17,8 +17,29 @@
 ;;
 ;; If the key is not found in the list, `assq` returns `#f`.
 
+(define operator-list
+  (list (list '+ +)
+        (list '- -)
+        (list '* *)
+        (list '/ /)
+        (list '> >)
+        (list '< <)
+        (list '>= >=)
+        (list '<= <=)
+        (list '= =)
+        (list 'x 10)
+        (list 'x2 100)
+        (list 'NEQ (lambda (x y) (not (= x y))))
+        (list 'ANND (lambda (x y) (and x y)))
+        (list 'ORR (lambda (x y) (or x y)))
+        (list 'NOTT not)))
 
+(define (my-assq key lst)
+  (cond [(null? lst) #f]
+        [(eq? key (first (first lst))) (first lst)] 
+        [else (my-assq key (rest lst))]))
 
+(display (rest (my-assq '* operator-list))) (newline) ;; --> <procedure:*>
 
 
 ;;;;;;;;;;;
@@ -28,3 +49,15 @@
 ;;
 ;; Add the `lookup-list` argument to your hw2 evaluator (or ours, from the solution set).
 ;; `(evaluate 'foo lookup-list)` should return whatever `'foo` is associated with in `lookup-list`.
+
+(define (evaluate expr)
+  (cond [(number? expr) expr]
+        [(and (not (list? expr)) (my-assq expr operator-list)) (second (my-assq expr operator-list))]
+        [(my-assq (first expr) operator-list) ((second (my-assq (first expr) operator-list)) (evaluate (second expr)) (evaluate (third expr)))]
+        [(eq? (first expr) 'IPH) (if (evaluate (second expr))
+                                     (evaluate (third expr))
+                                     (evaluate (fourth expr)))]
+        [(eq? (first expr) 'UM) (- (evaluate (second expr)))]
+        [else #f]))
+
+(display (evaluate '(IPH (<= (* (- x 2) (/ 9 3)) 0) (UM x2) (- (+ 7 8) 9)))) (newline) ;; --> 6
